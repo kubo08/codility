adf5b960168e29da6d56b13d5fac905bc3a59a5d
kubo08 <susedik.jakub@gmail.com>
2014-08-12 09:20:37 +0200

    max counter

---
+++ lesson 2 Counting Elements/MaxCounter.cs
@@ -0,0 +1,36 @@
+﻿namespace lesson_2_Counting_Elements
+{
+    class MaxCounter
+    {
+        public int[] solution(int N, int[] A)
+        {
+            int[] result= new int[N];
+            int max = 0;
+            // write your code in C# 5.0 with .NET 4.5 (Mono)
+            foreach (var item in A)
+            {
+                if (item == N + 1)
+                {
+                    result=SetAllToMax(result, max);
+                    continue;
+                }
+                result[item-1]++;
+                if (result[item-1] > max)
+                {
+                    max = result[item-1];
+                }
+            }
+            
+            return result;
+        }
+
+        private static int[] SetAllToMax(int[] counters, int max)
+        {
+            for (int i = 0; i < counters.Length; i++)
+            {
+                counters[i] = max;
+            }
+            return counters;
+        }
+    }
+}
