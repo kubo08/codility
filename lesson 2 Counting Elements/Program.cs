c6011c306b7f130bf163dccb608a06d129b44c97
kubo08 <susedik.jakub@gmail.com>
2014-08-12 09:07:33 +0200

    missing integer

--- lesson 2 Counting Elements/Program.cs
+++ lesson 2 Counting Elements/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace lesson_2_Counting_Elements
@@ -30,6 +31,36 @@ namespace lesson_2_Counting_Elements
             test = frog.solution(100000, A);
             timer.Stop();
             Console.WriteLine("frog jump: " + test);
+            Console.WriteLine(timer.Elapsed);
+
+            timer.Reset();
+
+            MaxCounter counters = new MaxCounter();
+            int[] numbers = new[] {3, 4, 4, 6, 1, 4, 4};
+            timer.Start();
+            var result = counters.solution(5, numbers);
+            timer.Stop();
+            Console.Write("max counters: ");
+            foreach (var item in result)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(timer.Elapsed);
+
+            timer.Reset();
+
+            //var array = new[] {1, 3, 6, 4, 1, 2};
+            var array = new List<int>();
+            for (int i = -2; i < 100000; i++)
+            {
+                array.Add(i);
+            }
+            MissingInteger miss=new MissingInteger();
+            timer.Start();
+            test=miss.solution(array.ToArray());
+            timer.Stop();
+            Console.WriteLine("missing integer: " + test);
             Console.WriteLine(timer.Elapsed);
         }
     }
