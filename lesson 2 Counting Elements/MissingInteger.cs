5b9ad14342387315d7e448419fca2e8cf123b576
kubo08 <susedik.jakub@gmail.com>
2014-08-12 09:19:58 +0200

    missing integer

---
+++ lesson 2 Counting Elements/MissingInteger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace lesson_2_Counting_Elements
+{
+    internal class MissingInteger
+    {
+        public int solution(int[] A)
+        {
+            List<int> list = A.Distinct().ToList();
+            list = list.Where(t => t > 0).ToList();
+            list.Sort();
+            for (int a = 1; a < list.Count + 1; a++)
+            {
+                if (!list.ElementAt(a - 1).Equals(a))
+                {
+                    return a;
+                }
+            }
+
+            return list.Count + 1;
+        }
+    }
+}
