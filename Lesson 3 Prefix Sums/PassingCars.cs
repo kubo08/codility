0f49bee549f7f81cfdbd00a00375b2af85d148b6
kubo08 <susedik.jakub@gmail.com>
2014-08-14 16:38:34 +0200

    lesson 3 - passing cars

---
+++ Lesson 3 Prefix Sums/PassingCars.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lesson_3_Prefix_Sums
+{
+    class PassingCars
+    {
+        public int solution(int[] A)    //100%
+        {
+            // write your code in C# 5.0 with .NET 4.5 (Mono)
+
+            int west = 0;
+            int count = 0;
+
+            for (int i = A.Length - 1; i >= 0; i--)
+            {
+                if (A[i] == 1)
+                {
+                    west++;
+                }
+                if (A[i] == 0)
+                {
+                    if (count + west > 1000000000)
+                    {
+                        return -1;
+                    }
+                    count += west;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Lesson 3 Prefix Sums/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Lesson_3_Prefix_Sums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PassingCars perm = new PassingCars();
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            var test = perm.solution(new[] {0, 1, 0, 1, 1});
+            timer.Stop();
+            Console.WriteLine("passing cars: " + test);
+            Console.WriteLine(timer.Elapsed);
+
+            timer.Reset();
+        }
+    }
+}
