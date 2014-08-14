17432685b0700915caaf3c86c7abf3d094cc6579
kubo08 <susedik.jakub@gmail.com>
2014-08-14 17:47:25 +0200

    genomic range query

---
+++ Lesson 3 Prefix Sums/GenomicRangeQuery.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lesson_3_Prefix_Sums
+{
+    class GenomicRangeQuery
+    {
+        public int[] solution(string S, int[] P, int[] Q)       //62% korektne, ale prrformance testy na 0%
+        {
+            // write your code in C# 5.0 with .NET 4.5 (Mono)
+            int length = P.Length;
+            int[] result = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                int minimum = 5;
+                for (int a = P[i]; a <= Q[i]; a++)
+                {
+                    switch (S[a])
+                    {
+                        case 'A':
+                            minimum = Math.Min(minimum, 1);
+                            break;
+                        case 'C':
+                            minimum = Math.Min(minimum, 2);
+                            break;
+                        case 'G':
+                            minimum = Math.Min(minimum, 3);
+                            break;
+                        case 'T':
+                            minimum = Math.Min(minimum, 4);
+                            break;
+                    }
+                }
+                result[i] = minimum;
+            }
+            return result;
+        }
+    }
+}
--- Lesson 3 Prefix Sums/Program.cs
+++ Lesson 3 Prefix Sums/Program.cs
@@ -10,13 +10,28 @@ namespace Lesson_3_Prefix_Sums
     {
         static void Main(string[] args)
         {
-            PassingCars perm = new PassingCars();
+            PassingCars cars = new PassingCars();
 
             Stopwatch timer = new Stopwatch();
             timer.Start();
-            var test = perm.solution(new[] {0, 1, 0, 1, 1});
+            var test = cars.solution(new[] { 0, 1, 0, 1, 1 });
             timer.Stop();
             Console.WriteLine("passing cars: " + test);
+            Console.WriteLine(timer.Elapsed);
+
+            timer.Reset();
+
+            GenomicRangeQuery query = new GenomicRangeQuery();
+
+            timer.Start();
+            var testArray = query.solution("CAGCCTA", new[] { 2,5,0 }, new []{4,5,6});
+            timer.Stop();
+            Console.Write("Genomic range query: ");
+            foreach (var item in testArray)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine();
             Console.WriteLine(timer.Elapsed);
 
             timer.Reset();
