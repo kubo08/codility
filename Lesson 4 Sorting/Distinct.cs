74f2f2b7929ce72668497d4e106671025198ac89
kubo08 <susedik.jakub@gmail.com>
2014-08-15 15:31:36 +0200

    added distinct

---
+++ Lesson 4 Sorting/Distinct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lesson_4_Sorting
+{
+    class Distinct
+    {
+        public int solution(int[] A)        //100%
+        {
+            // write your code in C# 5.0 with .NET 4.5 (Mono)
+            return GetDistinct(A).ToList().Count;
+        }
+
+        private IEnumerable<int> GetDistinct(int[] A)
+        {
+            HashSet<int> set = new HashSet<int>();
+            foreach (var num in A.Where(num => !set.Contains(num)))
+            {
+                set.Add(num);
+                yield return num;
+            }
+        }
+    }
+}
---
+++ Lesson 4 Sorting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lesson_4_Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
